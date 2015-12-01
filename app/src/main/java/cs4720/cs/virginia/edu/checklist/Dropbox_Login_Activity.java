79ae96532c42cfad0d68688cafb3d0f5a4b41c24
Rock Beom Kim <purasle@gmail.com>
2015-11-30 22:37:03 -0500

    you can list files now

--- app/src/main/java/cs4720/cs/virginia/edu/checklist/Dropbox_Login_Activity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/checklist/Dropbox_Login_Activity.java
@@ -4,20 +4,28 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.dropbox.client2.DropboxAPI;
@@ -42,18 +50,29 @@ public class Dropbox_Login_Activity extends AppCompatActivity {
     private DropboxAPI<AndroidAuthSession> mDBApi;
     private final static String FILE_DIR = "/SavedCheckLists/";
     private EditText fileName;
+
+    private ArrayList<String> itemList = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+
+    private final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            itemList.clear();
+            ArrayList<String> result = msg.getData().getStringArrayList("data");
+            itemList.addAll(result);
+            adapter.notifyDataSetChanged();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dropbox__login_);
 
-        tmp = (TextView) findViewById(R.id.test_output);
         fileName = (EditText) findViewById(R.id.fileName);
 
         Intent intent = getIntent();
         if (intent != null) {
             fromFile = intent.getStringExtra("toFile");
-            tmp.setText(fromFile);
 
         }
 
@@ -64,7 +83,34 @@ public class Dropbox_Login_Activity extends AppCompatActivity {
         AndroidAuthSession session = new AndroidAuthSession(appKeys);
         mDBApi = new DropboxAPI<AndroidAuthSession>(session);
         mDBApi.getSession().startOAuth2Authentication(Dropbox_Login_Activity.this);
+
+        ListView listView = (ListView) findViewById(R.id.fileList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+        listView.setOnItemClickListener(mMessageClickedHandler);
+        listView.setAdapter(adapter);
     }
+
+    private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            FileOutputStream outputStream = null;
+            String fileloc = FILE_DIR + itemList.get(position);
+            Log.i("File location", fileloc);
+            try {
+                File file = new File(fileloc);
+                outputStream = new FileOutputStream(file);
+                DropboxAPI.DropboxFileInfo info = mDBApi.getFile("/testing.txt", null, outputStream, null);
+            } catch (Exception e) {
+                System.out.println("Something went wrong: " + e);
+            } finally {
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {}
+                }
+            }
+
+        }
+    };
 
     @Override
     protected void onResume() {
@@ -88,6 +134,11 @@ public class Dropbox_Login_Activity extends AppCompatActivity {
         upload.execute();
     }
 
+    public void listFiles(View view) {
+        ListFilesDropbox list = new ListFilesDropbox(mDBApi, FILE_DIR,
+                handler);
+        list.execute();
+    }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
